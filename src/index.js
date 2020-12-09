1981ea8bc48e67281c6b1da525eea421bcbc46d9
Amgaa <amgaa.khosbayar@gmail.com>
2020-12-09 12:36:45 -0800

    after gramma png

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // src/index.js
 import "./styles/index.scss";
 import axios from 'axios';
+import gramma from './images/gramma3.png'
 
-const mic = document.querySelector('.mic')
-const onclick = document.querySelector('.on-click')
-const inputText = document.querySelector('.input-text')
-const outputText = document.querySelector('.output-text')
+const mic = document.querySelector('.mic');
+const onclick = document.querySelector('.on-click');
+const inputText = document.querySelector('.input-text');
+const outputText = document.querySelector('.output-text');
+
 
 const checkbtn = document.querySelector('.check-btn');
 const outputtext = document.querySelector('.output-text')
@@ -22,42 +24,75 @@ const fetchOutput = (sentenceCase) => {
     return axios.post(`/grammarcheck/?text=${sentenceCase}`)
 }
 
+function buildAssetPath(imgSrc) {
+    return `./${imgSrc}`
+}
+
+const correctTextAnswer = ['Gramma thinks you are correct', 'Gramma congratulate you on your proper use of English']
+
 if(chromeAgent) {
     // event listener
     mic.addEventListener('click', () => {
         recognition.start();
         onclick.innerText = 'Gramma is listening...';
     })
-    
-    checkbtn.addEventListener('click', () => {
-        outputtext.style.color = 'green'; 
-    })
-    
-    
-    // functions 
-    
     recognition.onsoundend = function() {
-        
         onclick.innerText = "";
     }
-    
-    
-    recognition.onresult = function(event) {
-        console.log(event)
-        const text = event.results[0][0].transcript;
 
+    const image = document.createElement('img');
+    image.classList.add('gramma-logo')
+    image.src = buildAssetPath(gramma);
+    const currentNav = document.querySelector('nav');
+    const currentdiv = document.getElementsByClassName('.logo');
+    // currentNav.insertBefore(image, currentdiv);
+    currentNav.prepend(image)
+
+    
+    function grammaSays() {
+        // const lang = speechSynthesisUtteranceInstance.lang = 'en-US';
+        
+        const speechText = correctTextAnswer[Math.floor(Math.random() * correctTextAnswer.length)]
+        console.log(speechText)
+        
+        const speech = new SpeechSynthesisUtterance();
+
+        speech.text = speechText;
+        speech.volume = 1;
+        speech.rate = 0.3;
+        speech.pitch = 1;
+
+         const synth = window.speechSynthesis;
+         synth.speak(speech);
+    }
+
+    recognition.onresult = function(event) {
+        // console.log(event)
+        const text = event.results[0][0].transcript;
         const sentenceCase = text[0].toUpperCase() + text.slice(1)
-        inputText.innerText = sentenceCase;
+
+        inputText.innerText = 'Gramma heard: ' + sentenceCase + ".";
 
         fetchOutput(sentenceCase)
         .then( output => {
             console.log(output)
-            outputText.innerText = output.data.matches[0].message;
-            
+            if(output.data.matches.length < 0) {
+                const solution = output.data.matches[0].replacements[0].value;
+                outputText.innerText = 'Gramma suggests using ' + '"' + solution + '"' + '.';
+            } else {
+                outputText.innerText = 'Gramma thinks you are correct';
+                
+            }
         });
-        
-       
     }
+
+    checkbtn.addEventListener('click', () => {
+        outputtext.innerText = 'Thank you for talking to Gramma.';
+        setInterval(() => {
+            inputText.innerText = '';
+            outputtext.innerText = '';
+        }, 2000) 
+    })
     
     recognition.onerror = function(event) {
         console.log(event);
